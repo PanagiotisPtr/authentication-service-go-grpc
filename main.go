09ae0dcf4a7f70e7aa49cfb7807b9d9ec8337576
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2022-05-15 16:17:19 +0300

    Added Dockerfile and env variable parameter to the auth service

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"panagiotisptr/authentication/protos"
 	"panagiotisptr/authentication/server"
+	"strconv"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -13,6 +15,17 @@ import (
 
 func main() {
 	logger := log.New(os.Stdout, "messaging-service", log.Lshortfile)
+	port := 80
+
+	portEnvVariable, foundPortEnvVariable := os.LookupEnv("PORT")
+	if !foundPortEnvVariable {
+		fmt.Println("Missing PORT env variable")
+		os.Exit(1)
+	}
+	port, err := strconv.Atoi(portEnvVariable)
+	if err != nil {
+		panic(err)
+	}
 
 	gs := grpc.NewServer()
 	cs, err := server.NewAuthenticationServer(logger)
@@ -24,10 +37,13 @@ func main() {
 
 	reflection.Register(gs)
 
-	list, err := net.Listen("tcp", ":8080")
+	address := fmt.Sprintf(":%d", port)
+	list, err := net.Listen("tcp", address)
 	if err != nil {
 		logger.Fatalf("Unable to listen. Error: %v", err)
 		os.Exit(1)
+	} else {
+		logger.Println("Listening on port " + address)
 	}
 
 	gs.Serve(list)
